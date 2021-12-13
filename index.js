0b50dcafdc5cf3a0d3f2c33a680d0f0069c3bf4c
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-13 22:22:01 +0000

    commit tree 5a3f7da520e3e78c79302d203f310c0284d7a9e9

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let max = Math.max(1, 2, 3, 4, 20);
+let num3 = 22
+let num4 = 6
 
-// Write your code here
+let mod = num3 % num4
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
